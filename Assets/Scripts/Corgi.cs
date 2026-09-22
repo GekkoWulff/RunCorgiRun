cc9ee1409c7156ac23b3617a35630d98a0adf3a4
GekkoWulff <butterflysting22@gmail.com>
2026-09-22 15:49:07 -0700

    Made input and movement and linked up to Corgi

--- Assets/Scripts/Corgi.cs
+++ Assets/Scripts/Corgi.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class Corgi : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    
+    //access sprite renderer
+    private SpriteRenderer corgiSpriteRenderer;
+
+    public void Awake()
     {
-        
+        corgiSpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Move(Vector2 direction)
     {
-        
+        corgiSpriteRenderer.transform.Translate(direction);
     }
+    //change position 
 }
